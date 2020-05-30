aa0f258d3b5315a8f40dd19222f8f10cbe0570ce
unknown <kirtanushah@gmail.com>
2020-05-29 19:02:21 -0700

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket){
