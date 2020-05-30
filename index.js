12576c69fcc4eddc030384f3564c1e6b22446308
kirtan-shah <kirtanushah@gmail.com>
2020-05-29 19:20:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket){
